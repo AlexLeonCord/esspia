cd1617355861bfadb2ca7da805b9443c3dc57512
AlexLeonCord <alexcord04@hotmail.com>
2021-09-15 11:00:02 -0500

    Esspia Feathers

--- src/models/cliente.model.js
+++ src/models/cliente.model.js
@@ -1,6 +1,9 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
 // for more of what you can do here.
+// for more of what you can do here.
+
+
 
 const Sequelize = require('sequelize');
 const DataTypes = Sequelize.DataTypes;
